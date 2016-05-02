18e2a3cb32d729eda1d65009e43958653ca8d2ce
Xiangyu Bu <xybu92@live.com>
2016-05-02 12:46:15 -0400

    Create install.sh

---
+++ scripts/oprofile/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# sudo apt-get install libpopt-dev libiberty-dev
+
+# cd /tmp
+# wget http://prdownloads.sourceforge.net/oprofile/oprofile-1.1.0.tar.gz
+# tar xvf oprofile-1.1.0.tar.gz
+# cd oprofile-1.1.0
+
+# ./configure
+# make
+# sudo make install
+
+sudo apt-get install oprofile
