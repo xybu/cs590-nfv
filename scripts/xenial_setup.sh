c630c473670b7ab9279e8370fcd376ee0308cf34
Xiangyu Bu <xybu92@live.com>
2016-09-24 02:16:27 -0400

    Create xenial_setup.sh

---
+++ scripts/xenial_setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get -y install ack-grep build-essential cmake automake gcc g++ valgrind curl vim-tiny gawk ssh libcurl4-openssl-dev openssl python-software-properties software-properties-common python3-dev python-dev git mercurial
