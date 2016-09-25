aeaf816f189839e7c5d13b12eeea0dc04bd01949
Xiangyu Bu <xybu92@live.com>
2016-09-25 16:47:33 -0400

    Update install.sh

--- scripts/qemu/install.sh
+++ scripts/qemu/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y qemu-kvm libvirt-bin bridge-utils virt-manager virt-top virt-what ubuntu-virt virtinst
+sudo apt-get install -y qemu-kvm virt-goodies virt-manager virt-top virt-viewer virt-what libvirt-bin libvirt-dev libvirt-doc libvirt-glib-1.0-dev bridge-utils
+# ubuntu-virt virtinst
