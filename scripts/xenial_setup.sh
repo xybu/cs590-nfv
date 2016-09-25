d880d96b1935f75a0052468eeb82a950562721b4
Xiangyu Bu <xybu92@live.com>
2016-09-25 00:58:12 -0400

    Update xenial_setup.sh

--- scripts/xenial_setup.sh
+++ scripts/xenial_setup.sh
@@ -31,3 +31,8 @@ git config --global core.editor vim
 rsync -zrvpE bu1@cap13.cs.purdue.edu:/home/bu1/.ssh /home/bu1/
 rsync -zrvpE bu1@cap13.cs.purdue.edu:/home/bu1/.bashrc /home/bu1/
 sudo cp -R /home/bu1/.ssh /root/
+
+# Fix cgroup issue.
+sed "s/GRUB_TIMEOUT=[0-9]*/GRUB_TIMEOUT=0/" /etc/default/grub | tee /tmp/grub
+sed "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cgroup_enable=memory swapaccount=1\"/" /tmp/grub | sudo tee /etc/default/grub
+sudo update-grub
